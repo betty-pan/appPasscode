918656145910307ca5e555e96cde4b6701df98c1
Betty Pan <betty19931218@gmail.com>
2021-01-28 14:13:11 +0800

    Initial Commit

--- appPasscode/ViewController.swift
+++ appPasscode/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet var numBtns: [UIButton]!
     @IBOutlet var passcodeImageViews: [UIImageView]!
     
-    var numbers = [0,1,2,3,4,5,6,7,8,9]
     var passcode = "1234"
     var entercode = ""
     
